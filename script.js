b726681b22d5ba437781dc026f33ef77f335bd76
Guilherme <gui_g88@hotmail.com>
2022-11-06 18:13:25 -0300

    third commit

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const socket = io('http://localhost:8888');
+// const socket = io('http://localhost:8888');
 
-console.log(socket);
+// console.log(socket);
 
-socket.emit('message', { msg: 'hello world' });
+// socket.emit('message', { msg: 'hello world' });
+
+// DOM
+
+// ________________________________________________________________________
+
+// constructor's name
+
+window; // Window
+
+const htmlDocument = window.document; // HTMLDocument
+
+const head = window.document.head; // HTMLHeadElement
+
+const body = window.document.body; // HTMLBodyElement
+
+const div = window.document.createElement('div'); // HTMLDivElement
+
+// console.log(window.constructor.name);
+
+// ________________________________________________________________________
+
+// node types
+
+window.document.querySelector('h1').nodeType; // ELEMENT_NODE
+
+window.document.querySelector('h1').firstChild.nodeType; // TEXT_NODE
+
+console.log(window.document.querySelector('#my-custom-comment').firstChild); // Element
+
+// ________________________________________________________________________
